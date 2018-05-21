f1ec794b9e7d03d1e110e33689d9b4e2bec83abd
Marcus Roberts <marcunc98@yahoo.com>
2018-05-21 17:07:27 -0400

    Adding FriendFinder hm

---
+++ app/data/friends.js
@@ -0,0 +1,65 @@
+var friendsArray = [
+  {
+    name: "Sally",
+    photo:
+      "http://www.gotceleb.com/wp-content/uploads/photos/salli-richardson/whitfield-queen-sugar-premiere-in-california/Salli-Richardson-Whitfield:-Queen-Sugar-Premiere--10.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Roselyn",
+    photo:
+      "https://latintrends.com/wp-content/uploads/2010/08/roselyn.jpg",
+    scores: [8, 7, 8, 4, 3, 8, 9, 8, 7, 1]
+  },
+  {
+    name: "Eva",
+    photo:
+      "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2017%2F11%2Feva-mendes-feature.jpeg%3Fw%3D450&w=700&q=85",
+    scores: [8, 9, 8, 9, 4, 8, 9, 8, 7, 7]
+  },
+  {
+    name: "Heidi",
+    photo:
+      "https://www.who2.com/wp-content/uploads/2015/10/heidiklum-6-272x408.jpg",
+    scores: [9, 4, 6, 3, 2, 8, 6, 4, 1, 3]
+  },
+  {
+    name: "Kellita",
+    photo:
+      "http://images.fandango.com/ImageRenderer/300/0/redesign/static/img/default_poster.png/0/images/masterrepository/performer%20images/p285048/kellitasmith-rubysbucketofblood.jpg",
+    scores: [5, 6, 3, 7, 9, 2, 5, 1, 4, 7]
+  },
+  {
+    name: "Gwen",
+    photo:
+      "https://media.brides.com/photos/580e652dcf602c7a3153f4e9/1:1/w_767/blogs-aisle-say-gwen-stefani-wears-diamond-engagement-ring-Main.jpg",
+    scores: [9, 8, 7, 7, 3, 1, 4, 8, 6, 2]
+  },
+  {
+    name: "Padma",
+    photo:
+      "http://i.perezhilton.com/wp-content/uploads/2016/03/padma-lakshmi-sexual-assault__oPt.jpg",
+    scores: [8, 4, 7, 7, 3, 1, 4, 3, 4, 9]
+  },
+  {
+    name: "Sloan",
+    photo:
+      "http://dlisted.com/wp-content/uploads/2017/09/wenn22696357.jpg",
+    scores: [4, 9, 8, 1, 3, 1, 7, 6, 4, 9]
+  },
+  {
+    name: "Meagan",
+    photo:
+      "http://dlisted.com/wp-content/uploads/2017/09/wenn22696357.jpg",
+    scores: [2, 7, 7, 2, 5, 5, 9, 6, 3, 8]
+  },
+  {
+    name: "Anita",
+    photo:
+      "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-img.essence.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1x1_lg%2Fpublic%2F1474920744%2FGettyImages-173147532_master.jpg%3Fitok%3DoFAIyGo8&w=700&q=85",
+    scores: [9, 9, 7, 2, 2, 7, 4, 8, 9, 9]
+  },
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
