1f79b33d7ad1cc89691c3071bd6ac7bc69ad600e
Marcus Roberts <marcunc98@yahoo.com>
2018-09-05 18:05:03 -0400

    updated changes to app

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,63 +1,75 @@
 var friendsArray = [
   {
-    name: "Sally",
+    name: "Rochelle Aytess",
     photo:
-      "http://www.gotceleb.com/wp-content/uploads/photos/salli-richardson/whitfield-queen-sugar-premiere-in-california/Salli-Richardson-Whitfield:-Queen-Sugar-Premiere--10.jpg",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      "http://www.dailyhoroscopes1.com/imgs/celeb/full/6882-3.jpg",
+    scores: [2, 1, 2, 3, 5, 1, 2, 1, 4, 1]
   },
   {
-    name: "Roselyn",
+    name: "Roselyn Sanchez",
     photo:
       "https://latintrends.com/wp-content/uploads/2010/08/roselyn.jpg",
     scores: [5, 1, 5, 4, 3, 5, 3, 5, 1, 1]
   },
   {
-    name: "Eva",
+    name: "Eva Mendes",
     photo:
       "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fpeopledotcom.files.wordpress.com%2F2017%2F11%2Feva-mendes-feature.jpeg%3Fw%3D450&w=700&q=85",
     scores: [3, 3, 3, 2, 1, 3, 4, 4, 5, 4]
   },
   {
-    name: "Heidi",
+    name: "Heidi Klum",
     photo:
       "https://www.who2.com/wp-content/uploads/2015/10/heidiklum-6-272x408.jpg",
     scores: [1, 5, 4, 3, 2, 2, 3, 4, 1, 3]
   },
   {
-    name: "Kellita",
+    name: "Kellita Smith",
     photo:
-      "http://images.fandango.com/ImageRenderer/300/0/redesign/static/img/default_poster.png/0/images/masterrepository/performer%20images/p285048/kellitasmith-rubysbucketofblood.jpg",
+      "https://m.media-amazon.com/images/M/MV5BMTI2ODM2MTIyNF5BMl5BanBnXkFtZTYwMjg3Mjg1._V1_.jpg",
     scores: [5, 5, 3, 5, 5, 2, 5, 1, 4, 4]
   },
   {
-    name: "Gwen",
+    name: "Gwen Stefani",
     photo:
       "https://media.brides.com/photos/580e652dcf602c7a3153f4e9/1:1/w_767/blogs-aisle-say-gwen-stefani-wears-diamond-engagement-ring-Main.jpg",
-    scores: [5, 5, 5, 5, 5, 1, 4, 5, 5, 2]
+    scores: [4, 4, 4, 4, 4, 1, 4, 5, 5, 2]
   },
   {
-    name: "Padma",
+    name: "Padma Lakshmi",
     photo:
-      "http://i.perezhilton.com/wp-content/uploads/2016/03/padma-lakshmi-sexual-assault__oPt.jpg",
+      "https://cdn-img.instyle.com/sites/default/files/styles/684xflex/public/images/2009/GalxMonth/092909-padma-lakshmi2-400_1.jpg?itok=9DETPJSv",
     scores: [2, 4, 4, 5, 3, 1, 4, 3, 4, 5]
   },
   {
-    name: "Sloan",
+    name: "Sloan Stephens",
     photo:
-      "http://dlisted.com/wp-content/uploads/2017/09/wenn22696357.jpg",
+      "http://cdn.punchng.com/wp-content/uploads/2018/03/31195930/Sloane-Stephens-.jpg",
     scores: [4, 5, 2, 1, 3, 1, 2, 5, 4, 4]
   },
   {
-    name: "Meagan",
+    name: "Meagan Good",
     photo:
       "http://www.celebplasticsurgeryonline.com/wp-content/uploads/2016/12/Meagan-Good-plastic-surgery-12.jpg",
     scores: [2, 5, 5, 2, 5, 5, 1, 1, 1, 2]
   },
   {
-    name: "Anita",
+    name: "Anita Baker",
     photo:
       "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-img.essence.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1x1_lg%2Fpublic%2F1474920744%2FGettyImages-173147532_master.jpg%3Fitok%3DoFAIyGo8&w=700&q=85",
     scores: [3, 5, 1, 4, 4, 3, 4, 5, 4, 5]
+  },
+  {
+    name: "Amarie",
+    photo:
+      " https://i.pinimg.com/originals/3d/1a/52/3d1a526d572cd56545e87c8cbc9aff34.jpg",
+    scores: [3, 3, 1, 4, 5, 3, 2, 5, 4, 3]
+  },
+  {
+    name: "Yaya DaCosta",
+    photo:
+      " https://vignette.wikia.nocookie.net/lawandorder/images/5/5d/YayaDacosta.jpg/revision/latest?cb=20150107211205",
+    scores: [1, 2, 1, 2, 5, 2, 2, 1, 1, 2]
   },
 ];
 
